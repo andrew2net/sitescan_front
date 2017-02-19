7cd027b317cd12861b99bdcc145d8485dbca0ce9
andrew2net <android.2net@gmail.com>
2017-02-19 21:23:51 +0100

    loader

--- lib/snapshot/renderer.rb
+++ lib/snapshot/renderer.rb
@@ -54,7 +54,7 @@ module Snapshot
       url += "?#{ fragment[:query] }" if fragment[:query].present?
 
       # Run PhantomJS
-      body = Rails.cache.fetch fragment, expires_in: 1.minute do
+      body = Rails.cache.fetch fragment, expires_in: 1.day do
         Tempfile.open 'page' do |temp|
           if Rails.env == 'development'
             # %x{phantomjs lib/snapshot/phantom-script.js #{ url }}
